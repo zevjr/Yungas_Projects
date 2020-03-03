880d7ba1488272fbee02dc8e59b1eb0f76e2a6cb
JoseCarlosVJr <josejunior.vieira@hotmail.com>
2020-03-03 14:11:47 -0300

    first commit

---
+++ yungas_tasks_backend/yungas_tasks_backend/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+
+
+from tasks import views
+
+router = routers.DefaultRouter()
+router.register(r'tasks', views.TaskViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
