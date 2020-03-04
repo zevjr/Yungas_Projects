5a4ba74ebfb073d603b847993137ad6db3baa80c
JoseCarlosVJr <josejunior.vieira@hotmail.com>
2020-03-04 10:08:07 -0300

    instalador de pacotes e inicializador do sistemas, via setup.py. Remoçao dos arquivos do docker. Adicionando o package.json

--- setup.py
+++ setup.py
@@ -9,6 +9,5 @@ if os.path.exists(node) and os.path.exists(venv):
 else:
     os.system('python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt')
     os.system('source venv/bin/activate && python3 yungas_tasks_backend/manage.py migrate')
-    os.system('cd yungas_tasks_frontend/ && npm install && npm install -S axios && npm install react-modal')
-    os.system('cd yungas_tasks_frontend/ && npm start &> /dev/null')
+    os.system('cd yungas_tasks_frontend/ && npm install && npm start &> /dev/null')
     os.system('nohup python3 yungas_tasks_backend/manage.py runserver localhost:8000')
